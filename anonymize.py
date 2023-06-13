39fd2468551f30dfb8fdd66d8f74a7f75e237b4e
unknown <roumamedj@gmail.com>
2023-06-13 21:49:32 +0100

    text file anonymization

--- anonymize.py
+++ anonymize.py
@@ -1,11 +1,49 @@
-from PyPDF2 import PdfReader
+import json
+from presidio_anonymizer import AnonymizerEngine
+from presidio_analyzer import AnalyzerEngine
+from presidio_anonymizer.entities import RecognizerResult, OperatorConfig
 
-reader=PdfReader("meta_projet_final.pdf")
 
+#read txt file
 text=''
-for i in range(len(reader.pages)):
-    page=reader.pages[i]
-    text += page.extract_text()
+with open("info.txt",'r',encoding='utf-8') as file:
+    text=file.read()
 
-with open("text_file",'w',encoding='utf-8') as file:
-    file.write(text)
+# 1/ Analyzer 
+#text = "I am louis, my second name is James Bond, my phone number is 07816093423"
+analyzer = AnalyzerEngine()
+analyzer_results = analyzer.analyze(text=text, language="en")
+
+# Analyzer output
+print("analyzer results")
+print(analyzer_results)
+
+# 2/Anonymizer
+
+# Initialize the engine:
+engine = AnonymizerEngine()
+
+#define customized opeartors
+operators={
+    "DEFAULT": OperatorConfig("replace", {"new_value": "<ANONYMIZED>"}),
+    "PHONE_NUMBER": OperatorConfig(
+        "mask",
+        {
+            "type": "mask",
+            "masking_char": "*",
+            "chars_to_mask": 12,
+            "from_end": True,
+        },
+    ),
+}
+# Invoke the anonymize function with the text,
+result = engine.anonymize(
+    text=text, analyzer_results=analyzer_results,operators=operators
+)
+
+print("Anonymized text")
+print(result.text)
+
+#storing the results
+with open('anonymized_text.txt','w',encoding='utf-8')as file:
+    file.write(result.text)
