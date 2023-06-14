56db8eee7584832d45e4ba7de8d5628632a3ec5e
romaissaMe <95141338+romaissaMe@users.noreply.github.com>
2023-06-14 18:06:36 +0100

    add anonymization  (#3)
    * initial commit
    
    * text file anonymization
    
    * anonymize-text
    
    * anonymize-text
    
    * anonymize-text
    
    * anonymize-text

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # legal-ai-assistant
+## anonymize.py
+Anonymizing text files by identifying and extracting different types of PII from text data and replacing sensitive information with generic placeholders or pseudonyms using Microsoft Presidio
+## installing requirements using pip
+pip install -r requirements.txt
---
+++ anonymize.py
@@ -0,0 +1,42 @@
+from presidio_anonymizer import AnonymizerEngine
+from presidio_analyzer import AnalyzerEngine
+from presidio_anonymizer.entities import OperatorConfig
+
+
+#read txt file
+text=''
+with open("text.txt",'r') as file:
+    text=file.read()
+
+# 1/ Analyzer 
+analyzer = AnalyzerEngine()
+analyzer_results = analyzer.analyze(text=text, language="en")
+
+
+# 2/Anonymizer
+
+# Initialize the engine:
+engine = AnonymizerEngine()
+
+#define customized opeartors
+operators={
+    "DEFAULT": OperatorConfig("replace", {"new_value": "<ANONYMIZED>"}),
+    "PHONE_NUMBER": OperatorConfig(
+        "mask",
+        {
+            "type": "mask",
+            "masking_char": "*",
+            "chars_to_mask": 12,
+            "from_end": True,
+        },
+    ),
+}
+# Invoke the anonymize function with the text,
+result = engine.anonymize(
+    text=text, analyzer_results=analyzer_results,operators=operators
+)
+
+
+#storing the results
+with open('anonymized_text.txt','w')as file:
+    file.write(result.text)
