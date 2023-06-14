d18f139cc3a5660b526109fb11a5cdfca43e1651
unknown <roumamedj@gmail.com>
2023-06-14 11:40:56 +0100

    anonymize-text

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # legal-ai-assistant
+## anonymize.py
+Anonymizing text files by identifying and extracting different types of PII from text data and replacing sensitive information with generic placeholders or pseudonyms using Microsoft Presidio
+## installing presidio using pip
+pip install presidio_analyzer
+pip install presidio_anonymizer
+python -m spacy download en_core_web_lg
--- anonymize.py
+++ anonymize.py
@@ -1,22 +1,17 @@
-import json
 from presidio_anonymizer import AnonymizerEngine
 from presidio_analyzer import AnalyzerEngine
-from presidio_anonymizer.entities import RecognizerResult, OperatorConfig
+from presidio_anonymizer.entities import OperatorConfig
 
 
 #read txt file
 text=''
-with open("info.txt",'r',encoding='utf-8') as file:
+with open("text.txt",'r') as file:
     text=file.read()
 
 # 1/ Analyzer 
-#text = "I am louis, my second name is James Bond, my phone number is 07816093423"
 analyzer = AnalyzerEngine()
 analyzer_results = analyzer.analyze(text=text, language="en")
 
-# Analyzer output
-print("analyzer results")
-print(analyzer_results)
 
 # 2/Anonymizer
 
@@ -41,9 +36,7 @@ result = engine.anonymize(
     text=text, analyzer_results=analyzer_results,operators=operators
 )
 
-print("Anonymized text")
-print(result.text)
 
 #storing the results
-with open('anonymized_text.txt','w',encoding='utf-8')as file:
+with open('anonymized_text.txt','w')as file:
     file.write(result.text)
