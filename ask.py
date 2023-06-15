20629e202b1326b50e0f79ed4e8bd87da28541af
Imad-Allal <imad.allal@outlook.com>
2023-06-15 19:58:54 +0100

    Correcting the line 7, the problem is now solved

--- ask.py
+++ ask.py
@@ -2,24 +2,30 @@ import os
 import openai
 from dotenv import load_dotenv
 
+
 load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
-OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+def main():
 
-messages=[
-            {"role": "system", "content": "You are a leagal assistant"},
-        ]
 
-while True:
-    message = input("User : ")
-    if message:
-        messages.append(
-            {"role": "user", "content": message},
-        )
-        chat = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=messages
-        )
-    
-    reply = chat.choices[0].message.content
-    print(f"ChatGPT: {reply}")
-    messages.append({"role": "assistant", "content": reply})
+
+    messages=[
+                {"role": "system", "content": "You are a leagal assistant"},
+            ]
+
+    while True:
+        message = input("User : ")
+        if message:
+            messages.append(
+                {"role": "user", "content": message},
+            )
+            chat = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo", messages=messages
+            )
+        
+        reply = chat.choices[0].message.content
+        print(f"ChatGPT: {reply}")
+        messages.append({"role": "assistant", "content": reply})
+
+main()        
