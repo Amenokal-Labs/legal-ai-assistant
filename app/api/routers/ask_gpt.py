d21fe6a415a2102ec48c8d92c2129902b435f89e
unknown <roumamedj@gmail.com>
2023-06-21 17:08:15 +0100

    ask-gpt-endpoint #2

--- app/api/routers/ask_gpt.py
+++ app/api/routers/ask_gpt.py
@@ -7,13 +7,13 @@ load_dotenv()
 ask_router=APIRouter()
 openai.api_key=os.getenv("OPENAI_API_KEY")
 
-@ask_router.get('/ask_gpt')
-def ask():
+@ask_router.get('/ask')
+def ask(question:str):
     resp=openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "How are you today?"}
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": question}
     ]
 )
     return {'response':resp['choices'][0]['message']['content']}
