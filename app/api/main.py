f5440f989c0c4452859b2c81dca81b1bacd41651
romaissaMe <roumamedj@gmail.com>
2023-06-30 09:44:32 +0100

    making feat/add-dockerfile matches feat/add-simple-frontend

--- app/api/main.py
+++ app/api/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.routers.anonymize_file import anonymize_router
 from app.api.routers.ask_gpt import ask_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(anonymize_router)
 app.include_router(ask_router)
