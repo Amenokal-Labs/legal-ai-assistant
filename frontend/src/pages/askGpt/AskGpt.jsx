2d377f7e11e3d782d4abd4fbc14bd0b6336c53ce
romaissaMe <roumamedj@gmail.com>
2023-07-07 13:14:25 +0100

    make front responsive

--- frontend/src/pages/askGpt/AskGpt.jsx
+++ frontend/src/pages/askGpt/AskGpt.jsx
@@ -1,8 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
+import ReactScrollableFeed from 'react-scrollable-feed'
 import './askGpt.css'
+
 
 function AskGpt() {
     const url= process.env.REACT_APP_URL
+    const textareaRef = useRef(null);
     
     const [form,setForm]=useState({
         prompt:'',
@@ -16,6 +19,17 @@ function AskGpt() {
         setForm({...form,prompt:e.target.value})
     }
 
+    const handleTextAreaResize = (e) => {
+        const textarea = textareaRef.current
+        textarea.style.height = '25px'
+        if(textarea.scrollHeight <= '70px'){
+            textarea.style.height = `${textarea.scrollHeight}px`
+        } else {
+            textarea.style.height='70px'
+            textarea.style.setProperty('-webkit-scrollbar-width', '5px');
+        }
+    };
+
     const handleFileChange=(e)=>{
         setForm({...form,file:e.target.files[0]})
     }
@@ -25,24 +39,30 @@ function AskGpt() {
     }
 
     const handleClick = async () => {
-        setGenerateResp(true)
-        const text = await anonymizeFile()
-        try{
-        const res = await fetch(`${url}/ask`,{
-        method:"POST",
-        headers: {
-        'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({"question":form.prompt,"text":text})
-        })
-        const data = await res.json()
-        console.log(data['response'])
-        setResponse(data['response'])
-        setGenerateResp(false)
-        } catch(err){
-        console.log(`error while sending request to ask endpoint ${err.message}`)
-        }
+        if(validate()){
+            setGenerateResp(true)
+            const prompt = form.prompt
+            const text = await anonymizeFile()
+            try{
+            setForm({...form,prompt:''})
+            textareaRef.current.style.height = '25px'
+            const res = await fetch(`${url}/ask`,{
+            method:"POST",
+            headers: {
+            'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({"question":prompt,"text":text})
+            })
 
+            const data = await res.json()
+            console.log(data['response'])
+            setResponse(data['response'])
+            setGenerateResp(false)
+            } catch(err){
+            console.log(`error while sending request to ask endpoint ${err.message}`)
+            }
+    
+        }
     }
 
     const anonymizeFile = async () =>{
@@ -61,26 +81,36 @@ function AskGpt() {
         }
     }
 
+    const validate = () => {
+        if (form.prompt && form.file) return true
+        else return false
+    }
+
     return (
     <div className='askGpt-wrapper'>
         <form className='ask-form' onSubmit={handleSubmit}>
             <div className='form-field-file'>
                 <label htmlFor='file' > Share Your Legal Document </label>
-                <input type="file" id='file' onChange={handleFileChange}/>
+                <input type="file" id='file' onChange={handleFileChange} required/>
             </div>
             { response && !generateResp?
-            <div>
-                <h4> Answer : </h4>
-                <div className="response-text">{response}</div>
-            </div>:null
-            }
+
+                <div className="response-text">
+                    <ReactScrollableFeed>
+                        <h4> Answer : </h4>
+                        <p>{response}</p>
+                    </ReactScrollableFeed>
+                </div> :null
+                
+            }   
+            
         
             {generateResp && 
-                <div><p>Loading The Answer...</p></div>
+                <div className='loading-text'><p>Loading The Answer...</p></div>
             }
-            <div className='form-field'>
+            <div className='form-text-field'>
                 <label htmlFor='prompt'> Your Question </label>
-                <textarea id='prompt' placeholder='Enter Your Question' value={form.prompt} onChange={handlePromptChange} />
+                <textarea id='prompt' ref={textareaRef} placeholder='Type Your Question' value={form.prompt} onChange={handlePromptChange} onKeyUp={handleTextAreaResize} required />
                 <button type='submit' onClick={handleClick}>Send</button>
             </div>
         </form>
