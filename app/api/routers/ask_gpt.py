92df55956554d206896978d751a74741117467e8
romaissaMe <95141338+romaissaMe@users.noreply.github.com>
2023-07-17 16:11:59 +0100

    use OpenAiEmbeddings and langchains to support large file (#49)
    * embeddings-to-support-large-file
    
    * commit #2
    
    * customize parameters

--- app/api/routers/ask_gpt.py
+++ app/api/routers/ask_gpt.py
@@ -3,7 +3,7 @@ import openai
 from fastapi import APIRouter
 from dotenv import load_dotenv
 from pydantic import BaseModel
-from app.utils.anonymize import anonymize_text
+from app.utils.large_files import use_embeddings
 
 load_dotenv()
 ask_router = APIRouter()
@@ -18,20 +18,6 @@ class AskRequest(BaseModel):
 def ask(request: AskRequest):
     text = request.text
     question = request.question
-    resp = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {"role": "system",
-            "content": "I want you to play Paralegal Ai expert. you receive anonymized legal documents from both "
-                        "attorneys and non-legislatives. You are the legal Ai assistant, you have to analyze them, "
-                        "understand every word and sentence, explain what is written there, extract complex concepts, "
-                        "and serve as a guide for beginners and experts alike. You will be asked questions about the "
-                        "document, your mission is to answer them with the highest accuracy possible, you must work "
-                        "through it by heart to deliver correct knowledge and information extracted from the document "
-                        "which align perfectly with the user enquiries about the file and to develop useful resources "
-                        "from your current and previous experiences that user can use and will be beneficial to him."},
-            {"role": "user", "content": text},
-            {"role": "user", "content": f'considering the document I sent answer the following question: {question}'}
-        ]
-    )
-    return {'response': resp['choices'][0]['message']['content']}
+    response=use_embeddings(text,question)
+    return {'response': response}
+    
---
+++ app/utils/large_files.py
@@ -0,0 +1,36 @@
+import os
+from dotenv import load_dotenv
+import langchain
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import FAISS 
+from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import OpenAI
+from langchain import PromptTemplate 
+from app.constants.template import template
+
+load_dotenv()
+
+langchain.embeddings.openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def use_embeddings(text: str,question:str):
+    # Splitting up the text into smaller chunks for indexing
+    text_splitter = CharacterTextSplitter(        
+        separator = "\n",
+        chunk_size = 400,
+        chunk_overlap  = 10, #striding over the text
+        length_function = len,
+    )
+    texts = text_splitter.split_text(text)
+    #create and store embeddings for each chunck 
+    embeddings = OpenAIEmbeddings()
+    docsearch = FAISS.from_texts(texts, embeddings)
+    chain = load_qa_chain(OpenAI(temperature=0.8),chain_type="stuff") # stuff all the docs in at once
+    #define customized prompt
+    prompt = PromptTemplate(input_variables=['context', 'question'], 
+                            output_parser=None, partial_variables={}, template=template, template_format='f-string', validate_template=True)
+    chain.llm_chain.prompt=prompt
+    docs = docsearch.similarity_search(question,k=10)
+    return chain.run(input_documents=docs, question=question)
+
+
