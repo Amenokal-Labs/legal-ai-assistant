26244470d7aead350ddf9bdfff6be8dc1a8c9ddd
romaissaMe <roumamedj@gmail.com>
2023-07-17 13:18:03 +0100

    customize parameters

--- app/utils/large_files.py
+++ app/utils/large_files.py
@@ -17,20 +17,20 @@ def use_embeddings(text: str,question:str):
     # Splitting up the text into smaller chunks for indexing
     text_splitter = CharacterTextSplitter(        
         separator = "\n",
-        chunk_size = 1000,
-        chunk_overlap  = 100, #striding over the text
+        chunk_size = 400,
+        chunk_overlap  = 10, #striding over the text
         length_function = len,
     )
     texts = text_splitter.split_text(text)
     #create and store embeddings for each chunck 
     embeddings = OpenAIEmbeddings()
     docsearch = FAISS.from_texts(texts, embeddings)
-    chain = load_qa_chain(OpenAI(),chain_type="stuff") # stuff all the docs in at once
+    chain = load_qa_chain(OpenAI(temperature=0.8),chain_type="stuff") # stuff all the docs in at once
     #define customized prompt
     prompt = PromptTemplate(input_variables=['context', 'question'], 
                             output_parser=None, partial_variables={}, template=template, template_format='f-string', validate_template=True)
     chain.llm_chain.prompt=prompt
-    docs = docsearch.similarity_search(question)
+    docs = docsearch.similarity_search(question,k=10)
     return chain.run(input_documents=docs, question=question)
 
 
